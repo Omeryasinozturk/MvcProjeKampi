3fdc1f5b4f902e3937aea84e8cb77f60ec22ca0a
omeryasinozturk <omeryasinozturk97@gmail.com>
2023-02-19 15:16:12 +0300

    Tema Düzenlemeleri

--- BusinessLayer/Concrete/CategoryManager.cs
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -1,4 +1,6 @@
-﻿using DataAccessLayer.Concrete.Repositories;
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete.Repositories;
 using EntityLayer.Concrete;
 using System;
 using System.Collections.Generic;
@@ -8,25 +10,40 @@ using System.Threading.Tasks;
 
 namespace BusinessLayer.Concrete
 {
-    public class CategoryManager
+    public class CategoryManager : ICategoryService
     {
-        GenericRepository<Category> repo = new GenericRepository<Category>();
+        ICategoryDal _categorydal;
 
-        public List<Category> GetAllBL()
+        public CategoryManager(ICategoryDal categorydal)
         {
-            return repo.List();
+            _categorydal = categorydal;
         }
 
-        public void CategoryAddBL(Category p)
+        public void CategoryAdd(Category category)
         {
-            if (p.CategoryName=="" || p.CategoryName.Length<=3 || p.CategoryDescription=="" || p.CategoryName>=51)
-            {
-                //Hata Mesajı
-            }
-            else
-            {
-                repo.Insert(p);
-            }
+            _categorydal.Insert(category);
         }
+
+        public List<Category> GetList()
+        {
+            return _categorydal.List();
+        }
+
+        //public void CategoryAddBl(Category p)
+        //{
+        //    if (p.CategoryName==""||p.CategoryStatus==false||p.CategoryName.Length<=2)
+        //    {
+        //        //Hata Mesajı
+        //    }
+        //    else
+        //    {
+        //        _categorydal.Insert(p);
+        //    }
+
+        //}
+
+
+
+
     }
 }
---
+++ BusinessLayer/ValidationRules/CategoryValidator.cs
@@ -0,0 +1,29 @@
+﻿
+using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class CategoryValidator:AbstractValidator<Category>
+    {
+        //Doğrulayıcılar (validators), form verisi (metin kutularına girilenler vb.)
+        //sunucuya gitmeden önce verilerin kontrol edilmesi için kullanılır.
+        //Bu şekilde hatalı verilerin sunucuya gelmeden değiştirilmesi sağlanarak
+        //hem güvenlik, hem de sunucuda yapılacak işler ve
+        //zaman açısından tasarruf sağlanır.
+        public CategoryValidator()
+        {
+            RuleFor(x => x.CategoryName).NotEmpty().WithMessage("Kategori Adını Boş Geçemezsiniz.");
+            RuleFor(x => x.CategoryDescription).NotEmpty().WithMessage("Açıklamayı Boş geçemezsiniz");
+            RuleFor(x => x.CategoryName).MinimumLength(3).WithMessage("Lütfen en az 3 karakter girişi yapınız.");
+            RuleFor(x => x.CategoryName).MaximumLength(20).WithMessage("Lütfen 20 karakterden fazla değer girişi yapmayınız.");
+
+
+        }
+    }
+}
